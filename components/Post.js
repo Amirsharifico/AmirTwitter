30d729b5a30c94c4e6138633784770e7370c8378
Amir <amirsharifico@gmail.com>
2022-11-24 20:41:49 -0500

    start of reply haftadoshish

--- components/Post.js
+++ components/Post.js
@@ -19,14 +19,15 @@ import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atom/modalAtom";
+import { modalState, postIdState  } from "../atom/modalAtom";
 import Moment from "react-moment";
 
 export default function Post({ post }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
-  const [open, setOpen] = useRecoilState(modalState)
+  const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -110,9 +111,19 @@ export default function Post({ post }) {
         {/* post image */}
         <img className="rounded-2xl mr-2" src={post.data().image} alt="" />
         {/* Icons */}
-        <div className="flex items-center select-none">
+        <div className="flex justify-between text-gray-500 p-2">
           {/* ***************************************************************************************************************  */}
-          <ChatIcon onClick={()=>setOpen(!open)} className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChatIcon
+            onClick={() => {
+              if (!session) {
+                signIn();
+              } else {
+                setPostId(post.id);
+                setOpen(!open);
+              }
+            }}
+            className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
+          />
           {/* ***************************************************************************************************************  */}
 
           {session?.user.uid === post?.data().id && (
