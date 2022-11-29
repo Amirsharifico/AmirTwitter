e978fedbd53e314eb3e08c17ce3a0f53df98edd5
Amir <amirsharifico@gmail.com>
2022-11-29 16:33:22 -0500

    read comment from firestone haftadonoh

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,18 +6,45 @@ import Widgets from "../../components/Widgets";
 import Post from "../../components/Post";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../../firebase";
+
+import Comment from "../../components/comment";
 
 export default function PostPage({ newsResults, randomUsersResults }) {
   const router = useRouter();
   const { id } = router.query;
   const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
 
   useEffect(
     () => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)),
     [db, id]
   );
+//******************************************************************************************** */
+useEffect(() => {
+  onSnapshot(
+    query(
+      collection(db, "posts", id, "comments"),
+      orderBy("timestamp", "desc")
+    ),
+    (snapshot) => setComments(snapshot.docs)
+  );
+}, [db, id]);
+
+
+
+//******************************************************************************************** */
+
+
+
+
   return (
     <div>
       <Head>
@@ -43,6 +70,17 @@ export default function PostPage({ newsResults, randomUsersResults }) {
           </div>
 
           <Post id={id} post={post} />
+          {comments.length > 0 && (
+            <div className="">
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
         </div>
 
         {/* Widgets */}
